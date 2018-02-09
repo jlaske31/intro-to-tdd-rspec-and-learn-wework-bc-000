5c5c083aa35a08f347e7453ccbb5e91f8e964879
Jordan Laske <jordan.laske@wework.com>
2018-02-09 00:29:12 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,7 @@
-age_of_person = current_age_of_birth_year(1984)
+require_relative '../current_age_for_birth_year.RUBY_VERSION
+
+describe "current_age_for_birth_year' do
+  age_of_person = current_age_for_birth_year(1984)
+  
 
 
